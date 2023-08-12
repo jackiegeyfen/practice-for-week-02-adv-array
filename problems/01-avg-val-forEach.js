ff73581af014af191e3615d27d20016090b18024
Jacqueline <jgeyfen@gmail.com>
2023-08-12 11:56:56 -0700

    problem 2 done

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -7,16 +7,21 @@ Solve this using Array's `forEach()` method.
 
 Examples:
 
-console.log(avgVal([5, 10])); // 7.5
-console.log(avgVal([3, 7, 2, 1, 2])); // 3
-console.log(avgVal([])); // null
 
 */
 
 let avgVal = function(arr) {
-    // Your code here
+  let sum=0
+  if (arr.length === 0) return null;
+  arr.forEach(function(el) {
+    (sum += el)
+  })
+  return sum/(arr.length);
 };
 
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
